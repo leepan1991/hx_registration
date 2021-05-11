a359076e1a4a17f0507eebc5a72c293167c992fd
victor.li <victor.li@cssinco.cn>
2021-05-11 17:16:25 +0800

    feat:add datetime in print log

--- hx_script.py
+++ hx_script.py
@@ -1,6 +1,13 @@
 from time_util import get_today
 from mitmproxy import ctx
+import datetime
 import time
+
+
+def get_date_time():
+    d = datetime.datetime.fromtimestamp(time.time())
+    time_str = d.strftime("%Y-%m-%d %H:%M:%S:%f")
+    return time_str
 
 
 def request(flow):
@@ -8,16 +15,16 @@ def request(flow):
     if request.url == 'https://huaxi2.mobimedical.cn/index.php?g=WapApi&m=Register&a=submitReg':
         cookies = dict(request.cookies)  # 转换cookies格式为dict
         urlencoded_form = dict(request.urlencoded_form)
-        print("SubmitReg Cookies:" + str(cookies))
-        print("SubmitReg Urlencoded_Form:" + str(urlencoded_form))
+        print(get_date_time() + "===>SubmitReg Cookies:" + str(cookies))
+        print(get_date_time() + "===>SubmitReg Urlencoded_Form:" + str(urlencoded_form))
         today_timestamp_eight = get_today()
-        current_time = int(time.mktime(time.time()))
+        current_time = int(time.time())
         time_diff = today_timestamp_eight + 15 - current_time
-        print("SubmitReg diff time: " + str(time_diff))
+        print(get_date_time() + "===>SubmitReg diff time: " + str(time_diff))
         if time_diff > 0:
-            print("正在等待时间到达8点，请稍后...")
+            print(get_date_time() + "===>正在等待时间到达8点，请稍后...")
             time.sleep(time_diff)
-        print("时间到达，开始执行...")
+        print(get_date_time() + "===>时间到达，开始执行...")
         # ctx.master.shutdown()
 
 
@@ -25,16 +32,25 @@ def response(flow):
     request = flow.request
     if request.url == 'https://huaxi2.mobimedical.cn/index.php?g=WapApi&m=Register&a=checkTime':
         text = flow.response.get_text()
-        print("CheckTime Old RESP: " + text)
+        print(get_date_time() + "===>CheckTime Old RESP: " + text)
         today_timestamp = get_today()
         text = text.replace(str(today_timestamp), str(today_timestamp - 45))
-        print("CheckTime New RESP: " + text)
+        print(get_date_time() + "===>CheckTime New RESP: " + text)
         flow.response.set_text(text)
 
     if request.url == 'https://huaxi2.mobimedical.cn/index.php?g=WapApi&m=Register&a=submitReg':
         text = flow.response.get_text()
-        print("SubmitReg RESP: " + text)
+        print(get_date_time() + "===>SubmitReg RESP: " + text)
+
+    if request.url == 'https://huaxi2.mobimedical.cn/index.php?g=WapApi&m=Register&a=doRegQueueResult':
+        text = flow.response.get_text()
+        print(get_date_time() + "===>DoRegQueueResult RESP: " + text)
+
+    if request.url == 'https://huaxi2.mobimedical.cn/index.php?g=WapApi&m=Register&a=realPayment':
+        text = flow.response.get_text()
+        print(get_date_time() + "===>RealPayment RESP: " + text)
 
 
 if __name__ == '__main__':
-    time.sleep(2.35624)
+    # time.sleep(2.35624)
+    get_date_time()
